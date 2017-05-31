68de53f052f318d8a36bb45083e26cdf6e0d660a
Vincent Ambo <tazjin@gmail.com>
2017-05-31 11:19:37 +0200

    refactor fish: Upgrade prompt config & modernise vars

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,25 +1,30 @@
-function fish_prompt
-   set_color --bold blue
-   echo -n (prompt_pwd)
-   set_color cyan
-   echo -n '> '
-end
+# Configure classic prompt
+set fish_color_user --bold blue
+set fish_color_cwd --bold white
+#set __fish_git_prompt_color green
+set __fish_git_prompt_showcolorhints yes
+set __fish_git_prompt_color_prefix purple
+set __fish_git_prompt_color_suffix purple
 
-function fish_right_prompt
-   set_color   magenta
-   echo -n (vcprompt -f "(%s:%b%a%m)")
-end
-
-set -gx EDITOR "emacsclient -t"
-set -gx ALTERNATE_EDITOR emacs
-set -gx VISUAL emacsclient
-set -gx SSH_AUTH_SOCK "$HOME/.gnupg/S.gpg-agent.ssh"
-
+# Fish configuration
 set fish_greeting ""
+set PATH $HOME/.local/bin $GOPATH/bin $PATH
 
-set PATH ~/bin/ /usr/local/bin /home/vincent/.gem/ruby/2.1.0/bin ~/.cabal/bin/ $PATH
+# Editor configuration
+set -gx EDITOR "emacsclient"
+set -gx ALTERNATE_EDITOR "emacs -q -nw"
+set -gx VISUAL "emacsclient"
 
+# SSH & GPG configuration
+set -gx SSH_AUTH_SOCK "$XDG_RUNTIME_DIR/ssh-agent.socket"
+
+# Miscellaneous
+eval (direnv hook fish)
+
+# Useful command aliases
 alias gpr 'git pull --rebase'
 alias gco 'git checkout'
 alias gf 'git fetch'
 alias gap 'git add -p'
+alias pbcopy 'xclip -selection clipboard'
+alias edit 'emacsclient -n'
